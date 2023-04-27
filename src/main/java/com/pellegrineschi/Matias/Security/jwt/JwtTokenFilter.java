2f5333d3e9125d73b982b4090238753c67ce805a
pellegrineschi <pellegrineschi86@gmail.com>
2023-04-26 23:19:10 -0300

    jwt token filter 3

--- src/main/java/com/pellegrineschi/Matias/Security/jwt/JwtTokenFilter.java
+++ src/main/java/com/pellegrineschi/Matias/Security/jwt/JwtTokenFilter.java
@@ -1,6 +1,9 @@
 
 package com.pellegrineschi.Matias.Security.jwt;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class JwtTokenFilter {
-    
+   private final static Logger logger = LoggerFactory.getLogger(JwtProvider.class);
 }
