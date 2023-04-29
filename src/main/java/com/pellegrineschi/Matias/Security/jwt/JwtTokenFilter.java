28a8b7b5cac7c29c685c744e3e1f267dc4490fec
pellegrineschi <pellegrineschi86@gmail.com>
2023-04-28 21:56:54 -0300

    jwt token filter 4

--- src/main/java/com/pellegrineschi/Matias/Security/jwt/JwtTokenFilter.java
+++ src/main/java/com/pellegrineschi/Matias/Security/jwt/JwtTokenFilter.java
@@ -1,9 +1,27 @@
 
 package com.pellegrineschi.Matias.Security.jwt;
 
+import com.pellegrineschi.Matias.Security.Service.UserDetailsImpl;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-public class JwtTokenFilter {
+public class JwtTokenFilter extends OncePerRequestFilter{
    private final static Logger logger = LoggerFactory.getLogger(JwtProvider.class);
+   
+   @Autowired
+   JwtProvider jwtProvider;
+   @Autowired
+   UserDetailsImpl userDatailsServiceImpl;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
